3ba50ed62aa074a7404b143b30eae81695c1621b
Idrees Mahmood <2324918m@student.gla.ac.uk>
2019-01-22 20:10:09 +0000

    error fixes

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,4 @@ def index(request):
     return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>about </a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>Index </a>")
+    return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>index </a>")
