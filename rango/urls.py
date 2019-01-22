8e50ea63e5f778d00ee6e2e2a6623925538fd550
Idrees Mahmood <2324918m@student.gla.ac.uk>
2019-01-22 20:03:26 +0000

    error fixes

--- rango/urls.py
+++ rango/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
-]
+    ]
