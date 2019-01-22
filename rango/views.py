e14ebea1e79d4f7dd87032c6083cd913c0ce8fe9
Idrees Mahmood <2324918m@student.gla.ac.uk>
2019-01-22 00:20:43 +0000

    error fixes

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 
 def index(request):
     return HttpResponse("Rango says hey there partner!<br/><a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page<br/><a href='/rango/'>Index</a>")
+    return HttpResponse(reverse('about'))
