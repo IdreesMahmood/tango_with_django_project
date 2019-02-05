f6943eb2896b20ffe38f2bfd2bb732578216612f
Idrees Mahmood <2324918m@student.gla.ac.uk>
2019-02-05 00:03:59 +0000

    complete chapter 4

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -103,8 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -125,3 +124,6 @@ USE_TZ = True
 
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
